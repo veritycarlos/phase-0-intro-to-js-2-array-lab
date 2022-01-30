b60d62c162bd3bb0a8e0cfe614ca4b4ecd36b85c
Verity Carlos <verity.carlos@gmail.com>
2022-01-29 18:54:34 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+const destructivelyAppendCat = (name) => {
+    cats.push(name);
+    return cats;
+}
+
+const destructivelyPrependCat = (name) => {
+    cats.unshift(name);
+    return cats;
+}
+
+const destructivelyRemoveFirstCat = (name) => {
+    cats.shift(name);
+    return cats;
+}
+
+const destructivelyRemoveLastCat = (name) => {
+    cats.pop(name);
+    return cats;
+}
+
+const appendCat = (name) => {
+    let newArray = [...cats];
+    newArray.push(name);
+    return newArray;
+}
+
+const prependCat = (name) => {
+    let newArray1 = [...cats];
+    newArray1.unshift(name);
+    return newArray1;
+}
+
+const removeLastCat = (name) => {
+    let newArray2 = [...cats];
+    newArray2.pop(name);
+    return newArray2;
+}
+
+const removeFirstCat = (name) => {
+    let newArray3 = [...cats];
+    newArray3.shift(name);
+    return newArray3;
+}
